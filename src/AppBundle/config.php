b09869b93885ec228b3fa4690c32eeaf50b2b75f
Laby Damaro CAMARA <ldamaro98@gmail.com>
2024-12-18 12:40:25 +0000

    config.php

---
+++ src/AppBundle/config.php
@@ -0,0 +1,13 @@
+?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'denombrement');
+define('DBPASS', 'M02p@5520242025');
+define('DBNAME', 'denombrement_principale');
+define('ENABLE_OAUTH', true);
+define('FILES_FOLDER', '/var/www/csweb-denomb/files');
+define('DEFAULT_TIMEZONE', 'Europe/Berlin');
+define('MAX_EXECUTION_TIME', '300');
+define('API_URL', 'http://denombrement.ins-denom.com/api/');
+define('CSWEB_LOG_LEVEL' , 'error');
+define('CSWEB_PROCESS_CASES_LOG_LEVEL', 'error');
+?>
